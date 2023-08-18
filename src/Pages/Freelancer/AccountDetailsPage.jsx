c03b2bbd11a0a603fe39d8aec8ae7da44850c8f8
yatharth-pdgt <yatharth@twopointone.com>
2023-08-18 15:54:19 -0700

    refactor: add linting and cleanup

--- src/Pages/Freelancer/AccountDetailsPage.jsx
+++ src/Pages/Freelancer/AccountDetailsPage.jsx
@@ -1,19 +1,20 @@
+import React from "react";
 import { Footer, LoggedInHeader, MyAccount } from "../../components";
 
 const AccountDetailsPage = () => {
-	return (
-		<div className="flex flex-col min-h-screen">
-			<div className="flex-grow">
-				<LoggedInHeader
-					includeNavBar={true}
-					category="JOBS"
-					isFreelancer={true}
-				/>
-				<MyAccount />
-			</div>
-			<Footer />
-		</div>
-	);
+    return (
+        <div className="flex flex-col min-h-screen">
+            <div className="flex-grow">
+                <LoggedInHeader
+                    includeNavBar={true}
+                    category="JOBS"
+                    isFreelancer={true}
+                />
+                <MyAccount />
+            </div>
+            <Footer />
+        </div>
+    );
 };
 
 export default AccountDetailsPage;
