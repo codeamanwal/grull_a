193c3723a1ea79e339dc71716d9413d59f193dd6
yatharth-pdgt <yatharth@twopointone.com>
2023-08-19 13:10:38 -0700

    chore: added precommit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,33 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'google',
+    'plugin:react/recommended',
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true,
+      },
+      'files': [
+        '.eslintrc.{js,cjs}',
+      ],
+      'parserOptions': {
+        'sourceType': 'script',
+      },
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+  ],
+  'rules': {
+    'no-console': 'off',
+  },
 };
--- src/Pages/Employer/BrowseFreelancersPage.jsx
+++ src/Pages/Employer/BrowseFreelancersPage.jsx
@@ -1,62 +1,62 @@
-import React from "react";
+import React from 'react';
 import {
-    BrowseFreelancers,
-    LoggedInHeader,
-    EditProfileCard,
-    Footer,
-} from "../../components";
-import { userProfile } from "../../components/Assets";
+  BrowseFreelancers,
+  LoggedInHeader,
+  EditProfileCard,
+  Footer,
+} from '../../components';
+import {userProfile} from '../../components/Assets';
 
 const BrowseFreelancersPage = () => {
-    return (
-        <div className="flex flex-col min-h-screen bg-[#1A0142]">
-            <LoggedInHeader
-                includeNavBar={true}
-                category="FREELANCER"
-                isFreelancer={false}
-            />
-            <div className="flex sm:flex-row space-y-6 flex-col space-x-6 ">
-                <div className="flex flex-col sm:space-y-4 space-y-6 font-bold sm:text-2xl text-white justify-start mt-4">
-                    <p className="sm:pl-20 text-center text-2xl font-bold">
+  return (
+    <div className="flex flex-col min-h-screen bg-[#1A0142]">
+      <LoggedInHeader
+        includeNavBar={true}
+        category="FREELANCER"
+        isFreelancer={false}
+      />
+      <div className="flex sm:flex-row space-y-6 flex-col space-x-6 ">
+        <div className="flex flex-col sm:space-y-4 space-y-6 font-bold sm:text-2xl text-white justify-start mt-4">
+          <p className="sm:pl-20 text-center text-2xl font-bold">
 						BROWSE FREELANCERS
-                    </p>
+          </p>
 
-                    <div className="p-2">
-                        {window.innerWidth <= 640 ? <BrowseFreelancers /> : null}
-                    </div>
+          <div className="p-2">
+            {window.innerWidth <= 640 ? <BrowseFreelancers /> : null}
+          </div>
 
-                    <div className="grid sm:grid-cols-4 sm:gap-20">
-                        <EditProfileCard
-                            toHire={true}
-                            isEmployerProfile={false}
-                            userProfileImg={userProfile}
-                            userName="Chandrakanth Sharma"
-                            profession="Product Designer"
-                        />
+          <div className="grid sm:grid-cols-4 sm:gap-20">
+            <EditProfileCard
+              toHire={true}
+              isEmployerProfile={false}
+              userProfileImg={userProfile}
+              userName="Chandrakanth Sharma"
+              profession="Product Designer"
+            />
 
-                        <EditProfileCard
-                            toHire={true}
-                            isEmployerProfile={false}
-                            userProfileImg={userProfile}
-                            userName="Chandrakanth Sharma"
-                            profession="Product Designer"
-                        />
+            <EditProfileCard
+              toHire={true}
+              isEmployerProfile={false}
+              userProfileImg={userProfile}
+              userName="Chandrakanth Sharma"
+              profession="Product Designer"
+            />
 
-                        <EditProfileCard
-                            toHire={true}
-                            isEmployerProfile={false}
-                            userProfileImg={userProfile}
-                            userName="Chandrakanth Sharma"
-                            profession="Product Designer"
-                        />
-                    </div>
-                </div>
-
-                {window.innerWidth > 640 ? <BrowseFreelancers /> : null}
-            </div>
-            <Footer />
+            <EditProfileCard
+              toHire={true}
+              isEmployerProfile={false}
+              userProfileImg={userProfile}
+              userName="Chandrakanth Sharma"
+              profession="Product Designer"
+            />
+          </div>
         </div>
-    );
+
+        {window.innerWidth > 640 ? <BrowseFreelancers /> : null}
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default BrowseFreelancersPage;
