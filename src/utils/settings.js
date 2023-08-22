bf675912eae1f12c1e6d22ef27f3d57855270ad4
yatharth-pdgt <yatharth@twopointone.com>
2023-08-21 20:03:49 -0700

    chore: config freeze false

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,7 +1,7 @@
 import config from 'react-global-configuration';
 
 function createSettings() {
-  config.set({'BACKEND_URL': process.env.REACT_APP_BACKEND_URL});
+  config.set({'BACKEND_URL': process.env.REACT_APP_BACKEND_URL}, {freeze: false});
 }
 
 export default createSettings;
