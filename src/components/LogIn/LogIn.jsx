f7b73c1edaf44246e05123d83de3f27cc61485d6
Srishti Lodhi <srishtilodhi38@gmail.com>
2023-10-21 17:17:08 +0530

    Signup changes

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -1,129 +1,109 @@
 /* eslint-disable */
-import React, {useState} from 'react';
-import PropTypes from 'prop-types';
-import { useLocation, useHistory } from 'react-router-dom';
-import config from 'react-global-configuration';
-import {Link} from 'react-router-dom';
-import {apple, facebook, google} from '../Assets';
-import Box from '../SignUp/Box';
-import {Formik, Form, Field, ErrorMessage} from 'formik';
-import LoggedInHeader from '../Header/LoggedInHeader';
-import { useNavigate } from 'react-router-dom';
-
-
-
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { useLocation, useHistory } from "react-router-dom";
+import config from "react-global-configuration";
+import { Link } from "react-router-dom";
+import { apple, facebook, google } from "../Assets";
+import Box from "../SignUp/Box";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import LoggedInHeader from "../Header/LoggedInHeader";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
-  const fieldClassNames = 'py-2 px-4 border text-xs w-72 h-10 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 m-2';
-  const errorClassNames = 'text-red-500 text-sm text-center';
+  const fieldClassNames = "py-2 px-4 border text-xs w-72 h-10 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 m-2";
+  const errorClassNames = "text-red-500 text-sm text-center";
   const [isIncorrectCredential, setIsIncorrectCredential] = useState(false);
   const location = useLocation();
   const params = new URLSearchParams(location.search);
-  const isFreelancerParam = params.get('isFreelancer');
-  const isFreelancer = isFreelancerParam === 'true';
+  const isFreelancerParam = params.get("isFreelancer");
+  const isFreelancer = isFreelancerParam === "true";
   const navigate = useNavigate();
 
-  
+  console.log("isFreelancer finally:", isFreelancer);
+  const urlEndpoint = `${config.get("BACKEND_URL")}/api/v0/auth/login`;
+  return (
+    <div>
+      <Formik
+        initialValues={{ email: "", password: "", checkbox: false }}
+        validate={values => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = "Email is required";
+          } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+            errors.email = "Invalid email address";
+          }
+          if (!values.password) {
+            errors.password = "Password is required";
+          }
+          if (!values.checkbox) {
+            errors.checkbox = "You must agree to Grull User Policy.";
+          }
+          return errors;
+        }}
+        onSubmit={async (values, { setSubmitting }) => {
+          const email = values.email;
+          const password = values.password;
 
-  console.log('isFreelancer finally:', isFreelancer);
-  const urlEndpoint = `${config.get('BACKEND_URL')}/api/v0/auth/login`;
-  return <div>
-
-    
-    <Formik
-      initialValues={{email: '', password: '', checkbox: false}}
-      validate={(values) => {
-        const errors = {};
-        if (!values.email) {
-          errors.email = 'Email is required';
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = 'Invalid email address';
-        }
-        if (!values.password) {
-          errors.password = 'Password is required';
-        }
-        if (!values.checkbox) {
-          errors.checkbox = 'You must agree to Grull User Policy.';
-        }
-        return errors;
-      }}
-      onSubmit={async (values, {setSubmitting}) => {
-        const email = values.email;
-        const password = values.password;
-
-        const urlencodedData = new URLSearchParams();
-        urlencodedData.append('username', email);
-        urlencodedData.append('password', password);
-        const result = await fetch(
-            urlEndpoint,
-            {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-              },
-              body: urlencodedData,
+          const urlencodedData = new URLSearchParams();
+          urlencodedData.append("username", email);
+          urlencodedData.append("password", password);
+          const result = await fetch(urlEndpoint, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
             },
-        );
-        setSubmitting(false);
-        if (!result.ok && result.status == 400) {
-          setIsIncorrectCredential(true);
-          return false;
-        }
+            body: urlencodedData,
+          });
+          setSubmitting(false);
+          if (!result.ok && result.status == 400) {
+            setIsIncorrectCredential(true);
+            return false;
+          }
 
-        if (!result.ok) {
-          const text = await result.text();
-          console.error(`Error(${result.status}): ${text}.`);
-          return false;
-        }
-        const responseData = await result.json();
-        localStorage.setItem('access_token', responseData['access_token']);
-        localStorage.setItem('token_type', responseData['token_type']);
-        navigate('/LoggedInPage', { state: { isFreelancer, category: isFreelancer ? 'FREELANCER' : 'JOBS' } });
-        return true;
-      }}
-    >
-      {({isSubmitting}) => (
-        <Form>
-          <div className="flex flex-col items-center">
-            <ErrorMessage name="email" component="div" className={errorClassNames}/>
-            <Field placeholder="Email Address" type="email" name="email" className={fieldClassNames}/>
-            <ErrorMessage name="password" component="div" className={errorClassNames}/>
-            <Field placeholder="Password" type="password" name="password" className={fieldClassNames}/>
-            <div className="flex items-center space-x-2">
-              <Field name="checkbox" type="checkbox" className="form-checkbox h-6 w-6 text-indigo-600"></Field>
-              <span className="text-black font-semibold text-sm m-2">
-            I agree to the Grull User Policy Agreement
-              </span>
+          if (!result.ok) {
+            const text = await result.text();
+            console.error(`Error(${result.status}): ${text}.`);
+            return false;
+          }
+          const responseData = await result.json();
+          localStorage.setItem("access_token", responseData["access_token"]);
+          localStorage.setItem("token_type", responseData["token_type"]);
+          navigate("/LoggedInPage", { state: { isFreelancer, category: isFreelancer ? "FREELANCER" : "JOBS" } });
+          return true;
+        }}>
+        {({ isSubmitting }) => (
+          <Form>
+            <div className="flex flex-col items-center">
+              <ErrorMessage name="email" component="div" className={errorClassNames} />
+              <Field placeholder="Email Address" type="email" name="email" className={fieldClassNames} />
+              <ErrorMessage name="password" component="div" className={errorClassNames} />
+              <Field placeholder="Password" type="password" name="password" className={fieldClassNames} />
+              <div className="flex items-center space-x-2">
+                <Field name="checkbox" type="checkbox" className="form-checkbox h-6 w-6 text-indigo-600"></Field>
+                <span className="text-black font-semibold text-sm m-2">I agree to the Grull User Policy Agreement</span>
+              </div>
+              <ErrorMessage name="checkbox" component="span" className={errorClassNames} />
+              {isIncorrectCredential ? <span className={errorClassNames}>Incorrect credentials.</span> : null}
+              <button
+                type="submit"
+                className="flex justify-center p-2 bg-gradient-to-r from-purple-500 via-indigo-600 to-purple-800 rounded-lg w-60 m-2 text-white font-medium text-2xl leading-10 font-GeneralSans"
+                disabled={isSubmitting}>
+                Log In
+              </button>
             </div>
-            <ErrorMessage name="checkbox" component="span" className={errorClassNames}/>
-            {
-              isIncorrectCredential?
-              (<span className={errorClassNames}>Incorrect credentials.</span>): null
-            }
-            <button
-              type="submit"
-              className="flex justify-center p-2 bg-gradient-to-r from-purple-500 via-indigo-600 to-purple-800 rounded-lg w-60 m-2 text-white font-medium text-2xl leading-10 font-GeneralSans"
-              disabled={isSubmitting}
-            >
-             Log In
-            </button>
-          </div>
-        </Form>
-      )}
-    </Formik>
-  </div>;
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 };
-
 
 const LogIn = () => {
   return (
     <div className="w-96 bg-white rounded-2xl border border-black py-6 mx-auto">
       <div className="flex flex-col items-center">
-        <p className="font-medium text-5xl font-spaceGrotesk text-purple-600">
-          Log In
-        </p>
+        <p className="font-medium text-5xl font-spaceGrotesk text-purple-600">Log In</p>
 
         {/* <div className="flex flex-col gap-y-4 mt-4">
           <Box
@@ -153,24 +133,19 @@ const LogIn = () => {
             logoHeight="2rem"
           />
         </div> */}
-        <div className="border-b-2 border-gray-300 w-80 m-2 font-GeneralSans"/>
+        <div className="border-b-2 border-gray-300 w-80 m-2 font-GeneralSans" />
       </div>
       <LoginForm />
       <div className="flex flex-col items-center">
         <p className="text-lg">
           Don&apos;t have an account?
-          <Link
-            href="/signup"
-          >
+          <a href="/signUpOption">
             <span className="text-purple-700"> Sign Up</span>
-          </Link>
+          </a>
         </p>
       </div>
     </div>
   );
 };
 
-
 export default LogIn;
-
-
