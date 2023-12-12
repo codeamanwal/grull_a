19f9502f75f09736cca6fe1db8b274a6b2cfff19
Yatharth Mathur <yatharth.mathur@gmail.com>
2023-12-12 19:02:40 +0530

    chore: add variable to enable and disable emailjs

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,5 +1,14 @@
 import config from 'react-global-configuration';
 
+function getBooleanFromEnv(key, defaultValue=false) {
+  if (process.env[key] === undefined) {
+    return defaultValue;
+  }
+
+  const value = process.env[key].toLowerCase();
+  return ['true', '1'].includes(value);
+}
+
 function createSettings() {
   config.set(
       {
@@ -7,6 +16,7 @@ function createSettings() {
         'EMAILJS_USER_ID': process.env.REACT_APP_EMAILJS_USER_ID,
         'EMAILJS_SERVICE_ID': process.env.REACT_APP_EMAILJS_SERVICE_ID,
         'EMAILJS_TEMPLATE_ID': process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        'EMAILJS_SERVICE_ENABLED': getBooleanFromEnv('REACT_APP_EMAILJS_SERVICE_ENABLED'),
       },
       {freeze: false},
   );
