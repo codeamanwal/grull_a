3ef805c6a87cc259b44dbdec91f636ee4c0dd31e
Yatharth Mathur <yatharth.mathur@gmail.com>
2023-12-12 18:33:31 +0530

    chore: rename email js related env variables

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -4,9 +4,9 @@ function createSettings() {
   config.set(
       {
         'BACKEND_URL': process.env.REACT_APP_BACKEND_URL,
-        'EMAILJS_USER_ID': process.env.REACT_APP_EMAIL_USER_ID,
-        'EMAILJS_SERVICE_ID': process.env.REACT_APP_SERVICE_ID,
-        'EMAILJS_TEMPLATE_ID': process.env.REACT_APP_TEMPLATE_ID,
+        'EMAILJS_USER_ID': process.env.REACT_APP_EMAILJS_USER_ID,
+        'EMAILJS_SERVICE_ID': process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        'EMAILJS_TEMPLATE_ID': process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
       },
       {freeze: false},
   );
